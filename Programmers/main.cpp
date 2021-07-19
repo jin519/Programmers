20bbb5a7822a0d81a10e586fdd50dd215ac8ffa9
godjin <godjin519@gmail.com>
2021-07-20 03:28:45 +0900

    [코딩테스트 고득점 Kit] 스택/큐_다리를 지나는 트럭

--- Programmers/main.cpp
+++ Programmers/main.cpp
@@ -2,51 +2,58 @@
 #include <vector>
 #include <queue>
 #include <iostream>
-#include <utility>
 
 using namespace std;
 
-int solution(vector<int> priorities, int location) 
+int solution(int bridge_length, int weight, vector<int> truck_weights) 
 {
-    queue<pair<int, int>> queue; 
-    priority_queue<int> pQueue; 
-    
-    for (size_t i = 0; i < priorities.size(); ++i) 
+    int time = 0;
+
+    queue<int> bridge;
+    for (size_t i = 0; i < bridge_length; ++i)
+        bridge.emplace(0); 
+
+    int sum = 0;
+    int i = 0; 
+
+    do
     {
-        const int priority = priorities[i]; 
-        
-        queue.emplace(priority, i); 
-        pQueue.emplace(priority); 
-    }
+        ++time;
 
-    int answer = 1;
+        // 트럭 한칸 이동
+        sum -= bridge.front();
+        bridge.pop();
 
-    while (true) 
-    {
-        const pair<int, int> front = queue.front(); 
-        queue.pop(); 
+        const int truck = truck_weights[i];
+        sum += truck;
 
-        if (front.first < pQueue.top()) 
-            queue.emplace(front.first, front.second); 
+        if (sum <= weight)
+        {
+            // 유효 트럭 배치
+            bridge.emplace(truck);
+            ++i; 
+        }
         else 
         {
-            if (location == front.second) 
-                break;
-
-            pQueue.pop();
-            ++answer;
+            // 빈 트럭 배치
+            bridge.emplace(0); 
+            sum -= truck;
         }
-    }
-    
-    return answer;
+    } while (i < truck_weights.size()); 
+
+    // 유효 트럭 전부 이동
+    time += bridge_length;
+
+    return time;
 }
 
 int main() 
 {
-    vector<int> priorities = { 1, 1, 9, 1, 1, 1 };
-    int location = 0;
+    int bridge_length = 2;
+    int weight = 10; 
+    vector<int> truck_weights = { 7,4,5,6 };
 
-    cout << solution(priorities, location); 
+    cout << solution(bridge_length, weight, truck_weights);
 
     return 0; 
 }
