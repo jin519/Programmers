945e0568c03b8b281670c62f6cfafd51ce72cd36
godjin <godjin519@gmail.com>
2021-07-19 22:02:56 +0900

    [코딩테스트 고득점 Kit] 스택/큐_프린터

--- Programmers/main.cpp
+++ Programmers/main.cpp
@@ -1,59 +1,52 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
-vector<int> solution(vector<int> progresses, vector<int> speeds) 
+int solution(vector<int> priorities, int location) 
 {
-    vector<int> answer;
-    int day = 0;
-    int count = 0; 
-
-    for (size_t i = 0; i < progresses.size(); ++i) 
+    queue<pair<int, int>> queue; 
+    priority_queue<int> pQueue; 
+    
+    for (size_t i = 0; i < priorities.size(); ++i) 
     {
-        int progress = progresses[i];
-        const int speed = speeds[i]; 
-
-        progress += (day * speed); 
-
-        if (progress >= 100)
-            ++count;
-        else 
-        {
-            if (count) 
-            {
-                answer.emplace_back(count);
-                count = 0;
-            }
-
-            while (progress < 100)
-            {
-                ++day;
-                progress += speed;
-            }
-
-            ++count;
-        }
+        const int priority = priorities[i]; 
+        
+        queue.emplace(priority, i); 
+        pQueue.emplace(priority); 
     }
 
-    if (count)
-        answer.emplace_back(count); 
+    int answer = 1;
 
+    while (true) 
+    {
+        const pair<int, int> front = queue.front(); 
+        queue.pop(); 
+
+        if (front.first < pQueue.top()) 
+            queue.emplace(front.first, front.second); 
+        else 
+        {
+            if (location == front.second) 
+                break;
+
+            pQueue.pop();
+            ++answer;
+        }
+    }
+    
     return answer;
 }
 
 int main() 
 {
-    //vector<int> progresses = { 93, 30, 55 };
-    vector<int> progresses = { 95, 90, 99, 99, 80, 99 };
-    //vector<int> speeds = { 1, 30, 5 };
-    vector<int> speeds = { 1, 1, 1, 1, 1, 1 };
+    vector<int> priorities = { 1, 1, 9, 1, 1, 1 };
+    int location = 0;
 
-    const vector<int>& answer = solution(progresses, speeds); 
-
-    for (const int elem : answer)
-        cout << elem << ' '; 
+    cout << solution(priorities, location); 
 
     return 0; 
 }
